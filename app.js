67a50805b989d191aacffb3f1623679e2aa8c689
beniA <b.avramson@gmail.com>
2017-10-01 18:11:20 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api', function(req, res) {
+    console.log("Enter get api handler");
+
+    var user_id = req.param('id');
+    var token = req.param('token');
+    var geo = req.param('geo');
+
+    res.send(user_id + ' ' + token + ' ' + geo);
+})
+
+app.post('/api', function(req, res) {
+    var user_id = req.body.id;
+    var token = req.body.token;
+    var geo = req.body.geo;
+    
+    //res.send(user_id + ' ' + token + ' ' + geo);
+    res.json({id: user_id, 
+        token : token,
+        geo: geo
+    });
+});
+
+app.listen(port);
+console.log("Server started on port ", port);
